2f796c8355a379ab116ff083b8a4f8016e609280
Ramon Chavez <rachavez1974@gmail.com>
2019-08-13 12:41:52 -0400

    Finish rails-generators-readme-online-web-sp-000

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,24 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params)
+    @post = Post.new(post_params(params[:post].keys))
     @post.save
     redirect_to post_path(@post)
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+    @post.update(post_params(params[:post].keys))
     redirect_to post_path(@post)
   end
 
   def edit
     @post = Post.find(params[:id])
   end
+
+  private
+    def post_params(*args)
+      params.require(:post).permit(*args)
+    end
+  
 end
