eab43dc59c0f12a2706fc707fe7e5065bf49aa92
joseph <josephtkao@gmail.com>
2020-12-11 15:00:55 -0600

    Done.

--- db/migrate/20201211205949_channge_post_status_data_type_to_string.rb
+++ db/migrate/20201211205949_channge_post_status_data_type_to_string.rb
@@ -1,4 +1,4 @@
-class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration[5.0]
+class ChanngePostStatusDataTypeToString < ActiveRecord::Migration[5.0]
   def change
     change_column :posts, :post_status, :string
   end
