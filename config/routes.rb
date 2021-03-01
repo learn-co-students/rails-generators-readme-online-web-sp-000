9e3f41e0984aba8f5b7351fd487f03f6b314c82a
A.J. Romaniello <ajiellodev@gmail.com>
2021-03-01 16:48:26 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'categories/show'
 
+  resources :admin, only: %i[dashboard stats financials settings]
+  resources :accounts, only: %i[index new show edit update create delete destroy]
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :categories
 end
